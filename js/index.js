c1d50b318761c0df36ed32aac72266be4cde351d
Fernanda <fernandaarg1715@gmail.com>
2019-03-30 09:53:33 -0600

    implementando funciones de tecla de escucha

--- js/index.js
+++ js/index.js
@@ -9,10 +9,50 @@ var dy = -2;
 
 var ballRadius = 10;
 
-// Esta funcion dibuja un circulo en la posicion x, y
-function drawBall(){
+// Paleta
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+
+// Detectar las teclas direccionales de izquierda/derecha
+var rightPressed = false;
+var leftPressed = false;
+
+// Agregar eventos de presionado y soltado de teclas
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// Esta funcion determina si se presiona una tecla
+function keyDownHandler(event) {
+  if (event.keyCode == 39) {
+   rightPressed = true;
+  } else if (event.keyCode == 37) {
+   leftPressed = true;
+  }
+}
+
+// Esta funcion determina si se suelta una tecla
+function keyUpHandler(event) {
+  if (event.keyCode == 39) {
+   rightPressed = false;
+  } else if (event.keyCode == 37) {
+   leftPressed = false;
+  }
+}
+
+// Esta funcion dibuja una paleta
+function drawPaddle() {
   context.beginPath();
-  context.arc(x, y, ballRadius, 0, Math.PI*2, false);
+  context.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  context.fillStyle = "#0095DD";
+  context.fill();
+  context.closePath();
+}
+
+// Esta funcion dibuja un circulo en la posicion x, y
+function drawBall() {
+  context.beginPath();
+  context.arc(x, y, ballRadius, 0, Math.PI*2);
   context.fillStyle = "#0095DD";
   context.fill();
   context.closePath();
@@ -21,30 +61,24 @@ function drawBall(){
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-// Se llama a la funcion de dibujar un circulo
+  // Se llama a la funcion de dibujar un circulo
   drawBall();
 
-  // Verificar si llego al limite de arriba
-  if(y +  dy < 0) {
+  // Se llama a la funcion de dibujar la paleta
+  drawPaddle();
+
+  // Verificar si llego al limite de arriba/abajo
+  if (y + dy < 0 || y + dy > canvas.height) {
     dy = -dy;
   }
 
-// Verificar si llego al limite de abajo
-  if(y +  dy > canvas.height) {
-    dy = -dy;
-  }
-
-  // Verificar si llego al limite de izquierdo
-  if(x + dx < 0) {
-    dx = -dx;
-  }
-
-  // Verificar si llego al limite de derecho
-  if(x + dx > canvas.width) {
+  // Verificar si llego al limite izquierdo/derecho
+  if (x + dx < 0 || x + dx > canvas.width) {
     dx = -dx;
   }
 
   x += dx;
   y += dy;
 }
+
 setInterval(draw, 10);
